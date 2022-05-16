81a16652e4272b28d6d34f142128d0038c8803b1
perelsob <82542437+perelsob@users.noreply.github.com>
2022-05-16 20:46:36 +0200

    app.py

--- app.py
+++ app.py
@@ -37,16 +37,16 @@ def background_thread(args):
          
         if args:
           w = dict(args).get('w')
-          dbV = dict(args).get('db_value')
+          db_value = dict(args).get('db_value')
           open_bool = dict(args).get('open_bool')
           P = dict(args).get('P')
           I = dict(args).get('I')
         else:
           w = 30
-          dbV = 'nieco'
           open_bool = 0
           P = 4
           I = 5
+          db_value = 'stop'
         
 #         print(args)
 #         print(open_bool)        
@@ -60,8 +60,7 @@ def background_thread(args):
           y = data[0]
           angle = data[1]
 
-            
-          print(dbV)
+       
           print(args)
           print(w)
 
@@ -69,32 +68,28 @@ def background_thread(args):
           count += 1
           dataCounter +=1
 
-          if dbV == 'start':
-            dataDict = {
+#           if dbV == 'start':
+          dataDict = {
                 "t": time.time(),
                 "x": count,
                 "w": w,
                 "y": y}
        
-            dataList.append(dataDict)
+          dataList.append(dataDict)
               
-            val = '{"x": ' + str(count) + ',"w": ' + str(w)+',"y": ' + str(y) + '}'
-            val_balik.append(val)
+          val = '{"x": ' + str(count) + ',"w": ' + str(w)+',"y": ' + str(y) + '}'
+          val_balik.append(val)
               
-          else:
+#           else:
               
               
-            if len(val_balik)>0:
+          if len(val_balik)>0:
               fo = open("static/files/test.txt","a+")    
                 # val = '[{"y": 0.6551787400492523, "x": 1, "t": 1522016547.531831}, {"y": 0.47491473008127605, "x": 2, "t": 1522016549.534749}, {"y": 0.7495528524284468, "x": 3, "t": 1522016551.537547}, {"y": 0.19625207463282368, "x": 4, "t": 1522016553.540447}, {"y": 0.3741884249440639, "x": 5, "t": 1522016555.543216}, {"y": 0.06684808042190538, "x": 6, "t": 1522016557.546104}, {"y": 0.17399442194131343, "x": 7, "t": 1522016559.54899}, {"y": 0.025055174467733865, "x": 8, "t": 1522016561.551384}]'
                 
-            
-                
               fo.write("%s\r\n" %val_balik)
               val_balik =[]
-                
-              
-            socketio.emit('my_data',{'x': count,'w': w,'y': y,'angle': angle},namespace='/test')
+          socketio.emit('my_data',{'x': count,'w': w,'y': y,'angle': angle},namespace='/test')
      
 
 
@@ -180,7 +175,11 @@ def test_message(messageI):
 @socketio.on('db_event', namespace='/test')
 def db_message(message):   
 #    session['receive_count'] = session.get('receive_count', 0) + 1 
-    session['db_value'] = message['value']    
+    session['db_value'] = message['value']
+    print(session['db_value'])
+    print(session['db_value']=='start')
+    print(str(session.get('db_value', 0)))
+    print("----------------------")
 #    emit('my_response',
 #         {'data': message['value'], 'count': session['receive_count']})
 
@@ -190,6 +189,11 @@ def open_event_request():
     
     configuration =  str(session.get('w', 0))+";"+str(session.get('P', 0))+";"+str(session.get('I', 0))
     
+    if (str(session.get('db_value', 0))=='start'):
+        configuration = "1;" + configuration
+    else:
+        configuration = "0;" + configuration
+    
     
     
 #     ser.write(str(session.get('w', 0)).encode('ascii'))
@@ -198,7 +202,7 @@ def open_event_request():
     
     ser.write(str(configuration).encode('ascii'))
     print(str(configuration).encode('ascii'))
-    print(configuration)
+    print(str(configuration))
 
     session['open_bool'] = 1
 
